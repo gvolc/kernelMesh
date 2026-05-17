f017eae83eb119a92d9b362fc233678b68c17688
oxxx1mif <pluginexteragram@gmail.com>
2026-05-17 14:19:19 +1000

    update: core/tun/src/lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Copyright Gleb Obitotsky <glebobitotsky@yandex.com> 2026.
+//
+// The code is distributed under the Mozilla Public License Version 2.0 license
+// (you can find the license file in the root folder).
+
 fn main() {
     println!("Hello, world!");
 }
