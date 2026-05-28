4fe49cd660ec8ce67ddba3dc5e72e7c1d78304e4
oxxx1mif <pluginexteragram@gmail.com>
2026-05-28 10:55:30 +1000

    updaate weight.go

--- common/a_star/weight.go
+++ common/a_star/weight.go
@@ -15,37 +15,43 @@ const maxLoss = 1.0
 
 // const maxPathCost = 1e9
 
-//type TelemetrySnapshot struct {
-//	Loss   float64
-//	Rtt    float64
-//	Load   float64
-//	Energy float64
-//}
+type TelemetrySnapshot struct {
+	Loss   float64
+	Rtt    float64
+	Load   float64
+	Energy float64
+}
 
 type GMetrics struct {
-	loss atomic.Uint64
+	//loss atomic.Uint64
 	//_      cpu.CacheLinePad
-	rtt atomic.Uint64
+	//rtt atomic.Uint64
 	//_      cpu.CacheLinePad
-	load atomic.Uint64
+	//load atomic.Uint64
 	//_      cpu.CacheLinePad
-	energy atomic.Uint64
+	//energy atomic.Uint64
 
-	//data atomic.Pointer[TelemetrySnapshot]
+	data atomic.Pointer[TelemetrySnapshot]
 }
 
-type Weight struct {
-	// Weight  atomic.Uint64
-	metrics GMetrics
+//type Weight struct {
+// Weight  atomic.Uint64
+// metrics GMetrics
+//}
+
+func NewGMetrics() *GMetrics {
+	gm := &GMetrics{}
+	gm.data.Store(&TelemetrySnapshot{})
+	return gm
 }
 
-func storeFloat(dst *atomic.Uint64, val float64) {
-	dst.Store(math.Float64bits(val))
-}
+//func storeFloat(dst *atomic.Uint64, val float64) {
+//	dst.Store(math.Float64bits(val))
+//}
 
-func loadFloat(src *atomic.Uint64) float64 {
-	return math.Float64frombits(src.Load())
-}
+//func loadFloat(src *atomic.Uint64) float64 {
+//	return math.Float64frombits(src.Load())
+//}
 
 func isValidTelemetry(val float64) bool {
 	return !math.IsNaN(val) && !math.IsInf(val, 0)
@@ -55,7 +61,19 @@ func (gm *GMetrics) UpdateLoss(val float64) {
 	if !isValidTelemetry(val) || val < 0 || val > maxLoss {
 		return
 	}
-	storeFloat(&gm.loss, val)
+	for {
+		current := gm.data.Load()
+		next := &TelemetrySnapshot{
+			Loss:   val,
+			Rtt:    current.Rtt,
+			Load:   current.Load,
+			Energy: current.Energy,
+		}
+		if gm.data.CompareAndSwap(current, next) {
+			return
+		}
+	}
+	//storeFloat(&gm.loss, val)
 	//bits := math.Float64bits(val)
 	//gm.loss.Store(bits)
 }
@@ -64,7 +82,19 @@ func (gm *GMetrics) UpdateRtt(val float64) {
 	if !isValidTelemetry(val) || val < 0 {
 		return
 	}
-	storeFloat(&gm.rtt, val)
+	for {
+		current := gm.data.Load()
+		next := &TelemetrySnapshot{
+			Loss:   current.Loss,
+			Rtt:    val,
+			Load:   current.Load,
+			Energy: current.Energy,
+		}
+		if gm.data.CompareAndSwap(current, next) {
+			return
+		}
+	}
+	//storeFloat(&gm.rtt, val)
 	//bits := math.Float64bits(val)
 	//gm.rtt.Store(bits)
 }
@@ -73,7 +103,19 @@ func (gm *GMetrics) UpdateLoad(val float64) {
 	if !isValidTelemetry(val) || val < 0 || val > 1.0 {
 		return
 	}
-	storeFloat(&gm.load, val)
+	for {
+		current := gm.data.Load()
+		next := &TelemetrySnapshot{
+			Loss:   current.Loss,
+			Rtt:    current.Rtt,
+			Load:   val,
+			Energy: current.Energy,
+		}
+		if gm.data.CompareAndSwap(current, next) {
+			return
+		}
+	}
+	//storeFloat(&gm.load, val)
 	//bits := math.Float64bits(val)
 	//gm.load.Store(bits)
 }
@@ -82,17 +124,26 @@ func (gm *GMetrics) UpdateEnergy(val float64) {
 	if !isValidTelemetry(val) || val < 0 || val > 1.0 {
 		return
 	}
-	storeFloat(&gm.energy, val)
+	for {
+		current := gm.data.Load()
+		next := &TelemetrySnapshot{
+			Loss:   current.Loss,
+			Rtt:    current.Rtt,
+			Load:   current.Load,
+			Energy: val,
+		}
+		if gm.data.CompareAndSwap(current, next) {
+			return
+		}
+	}
+	//storeFloat(&gm.energy, val)
 	//bits := math.Float64bits(val)
 	//gm.energy.Store(bits)
 }
 
-func (gm *GMetrics) GetLoss() float64 { return loadFloat(&gm.loss) }
+//func (gm *GMetrics) GetLoss() float64 { return loadFloat(&gm.loss) }
+//func (gm *GMetrics) GetRtt() float64 { return loadFloat(&gm.rtt) }
+//func (gm *GMetrics) GetLoad() float64 { return loadFloat(&gm.load) }
+//func (gm *GMetrics) GetEnergy() float64 { return loadFloat(&gm.energy) }
 
-func (gm *GMetrics) GetRtt() float64 { return loadFloat(&gm.rtt) }
-
-func (gm *GMetrics) GetLoad() float64 { return loadFloat(&gm.load) }
-
-func (gm *GMetrics) GetEnergy() float64 { return loadFloat(&gm.energy) }
-
-//func (gm *GMetrics) GetSnapshot() *TelemetrySnapshot { return gm.data.Load() }
+func (gm *GMetrics) GetSnapshot() TelemetrySnapshot { return *gm.data.Load() }
