9a0c70c0686ed35bbfaa42c9359b2c1f86915c54
oxxx1mif <pluginexteragram@gmail.com>
2026-05-28 17:54:12 +1000

    update rtt.go

--- common/a_star/rtt.go
+++ common/a_star/rtt.go
@@ -5,6 +5,11 @@
 
 package astar
 
+import (
+	"math"
+	"sync/atomic"
+)
+
 // Formula RTT:
 //
 // const a = 0.125
@@ -14,3 +19,49 @@ package astar
 // final formula coefficient RTT: RTT(coefficient) = RTT(smoothed) / 500.0
 //
 // 0.0 <= RTT(coefficient) <= 1.0
+
+const (
+	coefficientA = 0.125
+	maxPing      = 500.0
+)
+
+type Metrics struct {
+	RttOld         float64
+	RttNew         float64
+	RttSmoothed    float64
+	RttCoefficient float64
+}
+
+type Rtt struct {
+	data atomic.Pointer[Metrics]
+}
+
+func NewRtt() *Rtt {
+	rtt := &Rtt{}
+	rtt.data.Store(&Metrics{})
+	return rtt
+}
+
+func isValidRtt(val float64) bool {
+	return !math.IsNaN(val) && !math.IsInf(val, 0) && val >= 0.0
+}
+
+func (rtt *Rtt) UpdateRttOld(val float64) {
+	if !isValidRtt(val) {
+		return
+	}
+	for {
+		current := rtt.data.Load()
+		new := &Metrics{
+			RttOld:         val,
+			RttNew:         current.RttNew,
+			RttSmoothed:    current.RttSmoothed,
+			RttCoefficient: current.RttCoefficient,
+		}
+		if rtt.data.CompareAndSwap(current, new) {
+			return
+		}
+	}
+}
+
+func (rtt *Rtt) GetRtt() Metrics { return *rtt.data.Load() }
